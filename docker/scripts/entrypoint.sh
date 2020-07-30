e0aede9cdb062e85c4f8c67a0a06577d899816e9
Rakesh Vidyadharan <rakesh@sptci.com>
2020-07-30 07:48:12 -0500

    Updated docker entrypoint to also handle metrics database/collection configuration.

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -9,18 +9,6 @@ Check()
     echo "MONGO_URI must be set."
     exit 1
   fi
-
-  if [ -z "$VERSION_HISTORY_DATABASE" ]
-  then
-    VERSION_HISTORY_DATABASE="versionHistory"
-    echo "VERSION_HISTORY_DATABASE not set.  Will default to $VERSION_HISTORY_DATABASE"
-  fi
-
-  if [ -z "$VERSION_HISTORY_COLLECTION" ]
-  then
-    VERSION_HISTORY_COLLECTION="entities"
-    echo "VERSION_HISTORY_COLLECTION not set.  Will default to $VERSION_HISTORY_COLLECTION"
-  fi
 }
 
 Defaults()
@@ -35,6 +23,30 @@ Defaults()
   then
     THREADS=4
     echo "THREADS not set.  Will default to $THREADS"
+  fi
+
+  if [ -z "$VERSION_HISTORY_DATABASE" ]
+  then
+    VERSION_HISTORY_DATABASE="versionHistory"
+    echo "VERSION_HISTORY_DATABASE not set.  Will default to $VERSION_HISTORY_DATABASE"
+  fi
+
+  if [ -z "$VERSION_HISTORY_COLLECTION" ]
+  then
+    VERSION_HISTORY_COLLECTION="entities"
+    echo "VERSION_HISTORY_COLLECTION not set.  Will default to $VERSION_HISTORY_COLLECTION"
+  fi
+
+  if [ -z "$METRIC_DATABASE" ]
+  then
+    METRIC_DATABASE="versionHistory"
+    echo "METRIC_DATABASE not set.  Will default to $METRIC_DATABASE"
+  fi
+
+  if [ -z "$METRIC_COLLECTION" ]
+  then
+    METRIC_COLLECTION="metrics"
+    echo "METRIC_COLLECTION not set.  Will default to $METRIC_COLLECTION"
   fi
 }
 
@@ -51,6 +63,8 @@ Service()
     --mongo-uri $MONGO_URI \
     --version-history-database $VERSION_HISTORY_DATABASE \
     --version-history-collection $VERSION_HISTORY_COLLECTION \
+    --metric-database $METRIC_DATABASE \
+    --metric-collection $METRIC_COLLECTION \
     --port $PORT --threads $THREADS
 }
 
